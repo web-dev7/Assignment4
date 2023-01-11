5a962dcba11132a9bcf0838124cdb15e400ab9a2
vignesh <ssaivignesh7@gmail.com>
2023-01-11 15:45:46 +0530

    Add project files.

---
+++ ExtensionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension
+{
+  
+    public static class ExtensionClass
+    {
+        public static string ToCurrency(this string str)
+        {
+            try
+            {
+                int isNumber = Convert.ToInt32(str);
+                string outputString = "$" + isNumber;
+                return outputString;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+       
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1234";
+
+            string res = str.ToCurrency();
+
+            Console.WriteLine(res);
+        }
+    }
+}
